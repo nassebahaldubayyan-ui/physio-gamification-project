bcd554f732a3fe75384cbc352ed362bde45c0929
Nouf <no.alha.20@gmail.com>
2026-03-26 22:40:25 +0300

    EDIT

--- web_/api/views.py
+++ web_/api/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.hashers import make_password, check_password
 from django.utils import timezone
+from django.db import models
 from .models import Users, Messages, Patients, Doctors
 import json
 
